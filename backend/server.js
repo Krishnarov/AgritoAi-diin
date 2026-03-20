debd7dced2cb9f939d319297234a74f9e3ec77ff
krishnarov <krishnarovji@gmail.com>
2026-03-20 18:32:45 +0530

    Update CORS configuration to allow requests from the production URL

--- backend/server.js
+++ backend/server.js
@@ -22,7 +22,7 @@ const app = express();
 connectDB();
 
 // Middleware
-app.use(cors({ origin: "http://localhost:5173", credentials: true, methods: ["GET", "POST", "PUT", "DELETE", "PATCH"] }));
+app.use(cors({ origin: ["http://localhost:5173",'https://agrito-ai-diin.vercel.app'], credentials: true, methods: ["GET", "POST", "PUT", "DELETE", "PATCH"] }));
 app.use(express.json({ limit: "10mb" }));
 app.use(express.urlencoded({ extended: true }));
 
